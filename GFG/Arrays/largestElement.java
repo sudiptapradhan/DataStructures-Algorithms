168d702edc9ede07ef854324e631cd5a5d3d32f8
Sudipta Pradhan <64891121+sudiptapradhan@users.noreply.github.com>
2021-09-13 12:54:10 +0530

    largest element in an array

--- GFG/Arrays/largestElement.java
+++ GFG/Arrays/largestElement.java
@@ -5,7 +5,7 @@ class largestElement {
         
         Arrays.sort(arr);
         int large=0;
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             large=arr[n-1];
         }
         return large;
